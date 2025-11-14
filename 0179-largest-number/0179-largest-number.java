02308e7391a50ce84d69b9aeb5d67171837fe783
Pranjal Gupta <pranjal.gupta.872005@gmail.com>
2025-11-14 22:18:15 +0530

    Time: 6 ms (51.69%), Space: 44.7 MB (23.41%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -4,29 +4,11 @@ class Solution {
         for(int i=0;i<nums.length;i++)
             arr[i]=Integer.toString(nums[i]);
 
-        
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                String str1=arr[i]+arr[j];
-                String str2=arr[j]+arr[i];
+        Arrays.sort(arr,(a,b)->(b+a).compareTo(a+b));
 
-                if(str2.compareTo(str1)>0){
-                    //swap
-                    String temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
-                }
-            }    
-        }
-
-        String ans="";
-        for(String s:arr) ans+=s;
-
-        int flag=1;
-        for(int i=0;i<ans.length();i++){
-            if(ans.charAt(i) != '0') flag=0;
-        }
-        if(flag==1) return "0";
-        return ans;
+        if(arr[0].equals("0"))return "0";
+        StringBuilder sb=new StringBuilder();
+        for(String s: arr) sb.append(s); 
+        return sb.toString();
     }
 }

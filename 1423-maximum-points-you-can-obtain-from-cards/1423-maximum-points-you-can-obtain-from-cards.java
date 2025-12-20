f87f3c3f9c10a04ae1e755c0050c2bfa72b87290
Pranjal Gupta <pranjal.gupta.872005@gmail.com>
2025-12-20 17:51:21 +0530

    Time: 2 ms (99.44%), Space: 62.2 MB (10.07%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,37 +1,17 @@
 class Solution {
     public int maxScore(int[] cards, int k) {
-        int tot_score=0;
-        for(int i=0;i<cards.length;i++){
-            tot_score+=cards[i];
+        int lsum=0,rsum=0;
+        int max;
+        for(int i=0;i<k;i++) lsum+=cards[i];
+        max=lsum;
+        int ridx=cards.length-1;
+        for(int i=k-1;i>=0;i--){
+            lsum-=cards[i];
+            rsum+=cards[ridx--];
+            max=Math.max(max,lsum+rsum);
         }
-        if(k==cards.length) return tot_score;
-
-
-        int n=cards.length-k;
-        //find smallest subsequence of len n
-        int summ=Integer.MAX_VALUE;
-        int left=0;
-        int right=left+n-1;
-        while(right<cards.length){
-            summ=Math.min(summ,sum(cards,left,right));
-            left++;
-            right=left+n-1;
-        }
-        return tot_score-summ;
+        return max;
 
     }
-    private int sum(int[] cards,int left,int right){
-        int sum=0;
-        if((right-left)%2==0){
-            sum+=cards[left];
-            left++;
-        }
-        while(left<right){
-            sum+=cards[left];
-            sum+=cards[right];
-            left++;
-            right--;
-        }
-        return sum;
-    }
+
 }
